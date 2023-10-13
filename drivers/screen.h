eb41b7d90ec379ac508c0a33e091e82dc5373204
Hugo <hugo.winnberg@gmail.com>
2023-10-13 17:27:22 +0200

    Update screen.h

--- drivers/screen.h
+++ drivers/screen.h
@@ -30,6 +30,8 @@
 #define REG_SCREEN_CTRL 0x3d4
 #define REG_SCREEN_DATA 0x3d5
 
+extern int background_color;
+
 /* Public kernel API */
 void clear_screen();
 void kprint_at(char *message, int col, int row);
