18aa6fcc7af2e679eace598cad7abb7f9d470233
Hugo <hugo.winnberg@gmail.com>
2023-10-13 16:59:11 +0200

    Update screen.h

--- drivers/screen.h
+++ drivers/screen.h
@@ -3,6 +3,23 @@
 
 #include "../cpu/type.h"
 
+#define BLACK   0x00
+#define BLUE    0x10
+#define GREEN   0x20
+#define CYAN    0x30
+#define RED     0x40
+#define MAGENTA 0x50
+#define BROWN   0x60
+#define LIGHT_GRAY  0x70
+#define DARK_GRAY   0x80
+#define LIGHT_BLUE  0x90
+#define LIGHT_GREEN 0xA0
+#define LIGHT_CYAN  0xB0
+#define LIGHT_RED   0xC0
+#define LIGHT_MAGENTA 0xD0
+#define YELLOW      0xE0
+#define WHITE       0xF0
+
 #define VIDEO_ADDRESS 0xb8000
 #define MAX_ROWS 25
 #define MAX_COLS 80
