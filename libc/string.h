89151274d6014ef8f68f67fdad445a904d1d7b23
Hugo <hugo.winnberg@gmail.com>
2023-10-13 22:59:26 +0200

    Update string.h

--- libc/string.h
+++ libc/string.h
@@ -8,5 +8,8 @@ int strlen(char s[]);
 void backspace(char s[]);
 void append(char s[], char n);
 int strcmp(char s1[], char s2[]);
+int atoi(char *string);
+int is_numeric(const char* str);
+
 
 #endif
